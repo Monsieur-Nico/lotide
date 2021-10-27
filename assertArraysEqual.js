ddacb6a54061001702e71ddceed1c76bb8592d66
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-10-27 13:49:19 +0000

    assertArraysEqual edited

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,13 +4,13 @@ const failedLog = "[First Array] !=== [Second Array]"
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
-    return `🛑 Assertion Failed: ${failedLog}`;
+    return console.log(`🛑 Assertion Failed: ${failedLog}`);
   };
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
-      return `🛑 Assertion Failed: ${failedLog}`;
+      return console.log(`🛑 Assertion Failed: ${failedLog}`);
     }
   };
-  return `✅ Assertion Passed: ${passedLog}`;
+  return console.log(`✅ Assertion Passed: ${passedLog}`);
 };
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
